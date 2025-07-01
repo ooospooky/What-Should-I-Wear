2db681ae1c0554bbc6f392e67d6ee71766492803
JohnnyTai <bm414148@gmail.com>
2025-07-01 12:55:31 +0800

    refactor: migrate from Create React App to Vite with TypeScript

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'build'
+  }
+})
